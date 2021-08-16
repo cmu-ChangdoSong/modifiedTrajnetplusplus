b0d5aabcf9f573c54020a17d1768908c31ced96e
changdos <changdos@andrew.cmu.edu>
2021-08-17 00:53:11 +0900

    delete generated and cleanup unused lib

--- trajnetplusplusdataset/Python-RVO2-master/build/RVO2/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cds/gitDownload/trajnet++/trajnetplusplusdataset/Python-RVO2-master/src
-# Build directory: /home/cds/gitDownload/trajnet++/trajnetplusplusdataset/Python-RVO2-master/build/RVO2/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
